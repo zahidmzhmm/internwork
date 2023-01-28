55714226bbb395fb42a17bdd5cfc9da2132613da
Zahid Mzhmm <zahidmzhmm@gmail.com>
2023-01-28 22:36:03 +0600

    Some Changes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration {
             $table->id();
             $table->string('username');
             $table->string('email')->unique();
+            $table->integer('token')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
