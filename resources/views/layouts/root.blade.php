4067049e503261c0a1fe3156ba208f4414d14783
Zahid Mzhmm <zahidmzhmm@gmail.com>
2023-01-29 23:21:07 +0600

    Image Cropper in progress

--- resources/views/layouts/root.blade.php
+++ resources/views/layouts/root.blade.php
@@ -14,6 +14,7 @@
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}"/>
     @viteReactRefresh
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @yield('head')
 </head>
 
 <body>
@@ -168,5 +169,6 @@
     <script src="{{ asset('assets/js/jquery-3.3.1.min.js') }}"></script>
     <script src="{{ asset('assets/js/plugins.js') }}"></script>
     <script src="{{ asset('assets/js/main.js') }}"></script>
+@yield('footer')
 </body>
 </html>
--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -1,8 +1,41 @@
 @extends('layouts.root')
+@section('head')
+    <style>
+    </style>
+@endsection
 @section('content')
     <section class="page-title page-updated bg-dark">
     </section><!-- /.page-title -->
     <div class="container padding-main">
-        User Profile Panel
+        <div class="row">
+            <div class="col-sm-7 m-auto col-lg-5">
+                <div class="card">
+                    <div class="card-body profile">
+                        <form action="{{ route('profile.u.update') }}" method="post" enctype="multipart/form-data">
+                            <div class="mb-2 d-flex justify-content-center">
+                                <div class="border-5 image passport-image rounded-circle overflow-hidden">
+
+                                </div>
+                            </div>
+                            <div class="img-crop">
+                            </div>
+                            <input type="text" class="form-control mb-2" placeholder="First Name">
+                            <input type="text" class="form-control mb-2" placeholder="Last Name">
+                            <input type="text" class="form-control mb-2" placeholder="Mobile">
+                            <input type="text" class="form-control mb-2" placeholder="Email">
+                            <input type="text" class="form-control mb-2" placeholder="Institution">
+                            <input type="text" class="form-control mb-2" placeholder="Address">
+                            <div class="text-center mt-4">
+                                <button class="btn btn-info" type="submit">Update Profile</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+@endsection
+@section('footer')
+    <script>
+    </script>
 @endsection
