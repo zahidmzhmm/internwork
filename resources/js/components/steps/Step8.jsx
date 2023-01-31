5092a6fed553463003a500c43fe03cd86521f02e
Zahid Mzhmm <zahidmzhmm@gmail.com>
2023-01-31 17:55:31 +0600

    3 step in progress

---
+++ resources/js/components/steps/Step8.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Step8 = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Step8;
---
+++ resources/js/components/steps/Step9.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Step9 = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Step9;
